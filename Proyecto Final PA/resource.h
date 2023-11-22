dceb15931358eb4ac2b010e5d692edaeb4fd6be5
marce <marce@CHANGOPC>
2023-11-22 02:57:17 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Final PA/resource.h
@@ -0,0 +1,65 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Proyecto Final PA.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PROYECTOFINALPA_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PROYECTOFINALPA             107
+#define IDI_SMALL                       108
+#define IDC_PROYECTOFINALPA             109
+#define IDR_MAINFRAME                   128
+#define IDD_LOGGIN                      129
+#define IDD_INICIO                      130
+#define IDD_DIALOG1                     131
+#define IDR_MENU1                       132
+#define IDD_DIALOG2                     133
+#define IDD_DIALOG3                     134
+#define IDD_DIALOG4                     135
+#define IDC_INICIOSESION                1000
+#define IDC_EDIT1                       1002
+#define IDC_EDIT2                       1003
+#define IDC_COMBO1                      1005
+#define IDC_DATETIMEPICKER1             1006
+#define IDC_MONTHCALENDAR1              1007
+#define IDC_BUTTON1                     1008
+#define IDC_BUTTON2                     1009
+#define IDC_LIST1                       1011
+#define IDC_EDIT3                       1012
+#define IDC_COMBO2                      1013
+#define IDC_EDIT4                       1014
+#define IDC_RADIO2                      1016
+#define IDC_EDIT5                       1018
+#define IDC_EDIT6                       1019
+#define IDC_RADIO1                      1020
+#define IDC_RADIO3                      1021
+#define ID_AGENDAR_DARDEALTA            32771
+#define ID_AGENDAR_DARDEBAJA            32772
+#define ID_Menu                         32773
+#define ID_AGENDAR_CREARCITA            32774
+#define ID_AGENDAR_MODIFICA             32775
+#define ID_AGENDAR_ELIMINARCITA         32776
+#define ID_DOCTORES_NUESTROSDOCTORES    32777
+#define ID_NUESTROSDOCTORES_DR          32778
+#define ID_NUESTROSDOCTORES_DR32779     32779
+#define ID_NUESTROSDOCTORES_DRA         32780
+#define ID_AGENDA_AGENDAACTUAL          32781
+#define ID_SALIR_CERRARSESI32782        32782
+#define ID_DOCTORES_NUESTROSDOCTORES32783 32783
+#define ID_CITAS_CREARCITA              32784
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32785
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
